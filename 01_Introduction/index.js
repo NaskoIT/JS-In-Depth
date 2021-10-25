727b584b0a1c2faabe8de3ae77212a1cba0473c4
Atanas Vasilev <nasko01vasilev@gmail.com>
2021-10-25 23:46:30 +0300

    Primitive types, objects, functions, closure, function hoisting, arrays

--- 01_Introduction/index.js
+++ 01_Introduction/index.js
@@ -4,23 +4,23 @@
 var var0;
 var var1 = undefined; // undefined
 var var2 = 'string'; // string
-var var3 = 20; // number
-var var3 = true; // boolean
+var var3 = 20; // number ---> https://en.wikipedia.org/wiki/IEEE_754
+var var4 = true; // boolean
 var var5 = null; // null
 
 // Non-primitive values:
 
-var var6 = { prop1: 1000 }; // object
-var var7 = [ 1, 2, 3, 'string', [1, 2], {} ]; // array
-var var8 = function() { 
+var var6 = {prop1: 1000}; // object
+var var7 = [1, 2, 3, 'string', [1, 2], {}]; // array
+var var8 = function () {
     // do something
-}; //function
+}; //function (function expression)
 var8();
 
 // Immutable
 var var9 = var2 + '123';
-console.log(var3);
-console.log(var2);
+console.log(var3); // string123
+console.log(var2); // string
 
 var2[1] = 'a';
 console.log(var2); // "string"
@@ -42,65 +42,202 @@ console.log(typeof var6); // "object"
 console.log(typeof var7); // "object" ???
 console.log(typeof var8); // "function"
 
-String();
-Boolean();
-Array();
+// String();
+// Boolean();
+// Array();
 
-isNaN('dsda'); // true
+isNaN('dsda'); // true (bug)
 Number.isNaN('asdsa'); // false
+
+// Falsy values: false, NaN, null, undefined, '', 0
+
+console.log(4 == '4'); // true -> https://262.ecma-international.org/5.1/#sec-11.9.3
+console.log(4 === '4'); // false -> https://262.ecma-international.org/5.1/#sec-11.9.6
 
 var test = null;
 if (test == undefined) {
     console.log('OMG');
 }
+// OMG
 
-if(test === undefined || test === null) {
+if (test === undefined || test === null) {
     console.info('HELLO!');
 }
+//HELLO!
 
 var str = '123';
-if(typeof str === 'string') {
+if (typeof str === 'string') // NOT GOOD // https://262.ecma-international.org/5.1/#sec-7.9
+{
     console.log('string');
+} else if (true) {
+
+} else {
+
 }
+// string
 
 function doSomething() {
     console.log('do something')
     // do something
 } // function declaration
 
-function test(fn) {
-    return function() {
-        return function() {
+function testFunction(fn) {
+    return function () {
+        return function () {
             return fn();
         }
     }
 }
 
-test()()(doSomething);
+testFunction(doSomething)()(); // do something
 
-var testVar
+// ECMAScript 5
+// Primitive values;
 
-function testFn() {
-    var best = 333;
-    console.log(best);
-    console.log(best);
+var var0;
+var var1 = undefined; // undefined
+var var2 = 'string'; // string
+var var3 = 20; // number ---> https://en.wikipedia.org/wiki/IEEE_754
+var var4 = true; // boolean
+var var5 = null; // null
+
+// Non-primitive values:
+
+var var6 = {prop1: 1000}; // object
+var var7 = [1, 2, 3, 'string', [1, 2], {}]; // array
+var var8 = function () {
+    // do something
+}; //function (function expression)
+var8();
+
+// Immutable
+var var9 = var2 + '123';
+console.log(var3); // string123
+console.log(var2); // string
+
+var2[1] = 'a';
+console.log(var2); // "string"
+
+// Non immutable
+var var10 = var6;
+var10.test = 123;
+
+console.log(var10); // { prop1: 1000, test: 123 } --- Same object in the memory
+console.log(var6); // { prop1: 1000, test: 123 }
+
+console.log(typeof var0); // "undefined"
+console.log(typeof var1); // "undefined"
+console.log(typeof var2); // "string"
+console.log(typeof var3); // "number"
+console.log(typeof var4); // "boolean"
+console.log(typeof var5); // "object" ???
+console.log(typeof var6); // "object"
+console.log(typeof var7); // "object" ???
+console.log(typeof var8); // "function"
+
+// String();
+// Boolean();
+// Array();
+
+isNaN('dsda'); // true (bug)
+Number.isNaN('asdsa'); // false
+
+// Falsy values: false, NaN, null, undefined, '', 0
+
+console.log(4 == '4'); // true -> https://262.ecma-international.org/5.1/#sec-11.9.3
+console.log(4 === '4'); // false -> https://262.ecma-international.org/5.1/#sec-11.9.6
+
+var test = null;
+if (test == undefined) {
+    console.log('OMG');
+}
+// OMG
+
+if (test === undefined || test === null) {
+    console.info('HELLO!');
+}
+//HELLO!
+
+var str = '123';
+if (typeof str === 'string') // NOT GOOD // https://262.ecma-international.org/5.1/#sec-7.9
+{
+    console.log('string');
+} else if (true) {
+
+} else {
+
+}
+// string
+
+function doSomething() {
+    console.log('do something')
+    // do something
+} // function declaration
+
+function testFunction(fn) {
+    return function () {
+        return function () {
+            return fn();
+        }
+    }
 }
 
-function createGenerator() {
-    var i = 0;
-    return function generateNumber(i) {
-        return i++;
-    };
-};
+testFunction(doSomething)()(); // do something
+
+// Closure example
+var testValue = 100;
+function closureFn() {
+    var best = 666;
+    console.log(testValue); // undefined
+    // try to comment testValue and see the difference
+    var testValue = 100;
+    console.log(best); // 666
+
+    function insideClosureFn() {
+        console.log(best); // 666
+        console.log(testValue); // 100
+    }
+
+    insideClosureFn();
+}
+
+closureFn();
+
+// { } do not create scope like other languages like C#m Java, C++ ...
+{
+    var a = 1;
+    console.log(a); // 1
+}
+
+console.log(a); // 1
 
 var generator = createGenerator();
-console.log(generator());
-console.log(generator());
-console.log(generator());
+console.log(generator()); // 0
+console.log(generator()); // 1
+console.log(generator()); // 2
+
+var generator2 = createGenerator();
+console.log(generator2()); // 0
+console.log(generator2()); // 1
+
+// function hoisting
+function createGenerator() {
+    var i = 0;
+    return function generateNumber() {
+        return i++;
+    };
+}
+
+console.log(laterDefineVariable); // undefined
+var laterDefineVariable = 100;
+console.log(laterDefineVariable); // 100
 
 var consoleLogger = createLogger(console.log);
 consoleLogger('JS is cool!');
+// Logger created
+// JS is cool!
 
+// pass function as argument
 function createLogger(loggingFunction) {
     console.log('Logger created!');
     return loggingFunction;
@@ -112,22 +249,30 @@ function customLoggingFn(str) {
 
 var customLogger = createLogger(customLoggingFn);
 customLogger('Pass function as argument!');
+// Logger created
+// Custom logger: Pass function as argument!
+
+// fnSavedInVariable(); - VM338:1 Uncaught TypeError: fnSavedInVariable is not a function
+var fnSavedInVariable = function () {
+    console.log('In this way the function is not hoisted!');
+}
+fnSavedInVariable(); // In this way the function is not hoisted!
 
 var result = [1, 2, 3]
     .map(element => element + 1)
-    .filter(element => element % 2 == 0)
+    .filter(element => element % 2 === 0)
     .map(element => element + 2);
 
-console.log(result);
+console.log(result); // [4, 6]
 
 var reducedArray = [1, 2, 3]
-    .reduce((accumolator, currentElement) => {
+    .reduce((accumulator, currentElement, currentIndex, array) => {
         var num = currentElement + 1;
-        if(num % 2 === 0) {
-            return accumolator.concat(num + 2);
+        if (num % 2 === 0) {
+            return accumulator.concat(num + 2);
         }
 
-        return accumolator;
+        return accumulator;
     }, []);
 
-console.log(reducedArray);
+console.log(reducedArray); // [4, 6]
